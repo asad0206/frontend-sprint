515d8d71a42a54c592800a2df42cc415a733664a
Asad <asad200ana@gmail.com>
2023-05-07 13:47:49 +0530

    change

--- src/views/Home2.vue
+++ src/views/Home2.vue
@@ -1,61 +1,3 @@
-<!-- <template>
-  <v-container align-self="center" justify="center">
-    <v-row align-self="auto" justify="center">
-      <v-col cols="12" md="6">
-        <v-card v-for="(post, index) in posts" :key="index" class="mb-5">
-          <v-card-title>
-            <h3>{{ post.title }}</h3>
-          </v-card-title>
-          <v-card-text>
-            <p>{{ post.content }}</p>
-          </v-card-text>
-          <v-card-actions>
-            <v-row no-gutters align-self="center">
-              <v-col>
-                <v-btn icon v-on:click="upvote(index)">
-                  <v-icon>mdi-thumb-up</v-icon>
-                </v-btn>
-                <span>{{ post.upvotes }}</span>
-                <v-btn icon v-on:click="downvote(index)">
-                  <v-icon>mdi-thumb-down</v-icon>
-                </v-btn>
-                <span>{{ post.downvotes }}</span>
-              </v-col>
-              <v-col class="text-right">
-                <v-btn text color="primary" @click="show = !show"
-                  ><v-icon>mdi-comment</v-icon>
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-card-actions>
-          <v-expand-transition>
-            <div v-show="show">
-              <v-divider></v-divider>
-              <v-card-text
-                title="Comments"
-                v-for="(comments, index) in posts"
-                :key="index"
-                class="mb-5"
-              >
-                <ul>
-                  {{
-                    comments.comments
-                  }}
-                </ul>
-              </v-card-text>
-            </div>
-          </v-expand-transition>
-        </v-card>
-      </v-col>
-      <v-col>
-        <v-btn fab dark color="primary" v-on:click="createPost">
-          <v-icon>mdi-plus</v-icon>
-        </v-btn>
-      </v-col>
-    </v-row>
-  </v-container>
-</template> -->
-
 <template>
   <v-container align-self="center" justify="center">
     <v-row align-self="auto" justify="center">
@@ -80,18 +22,26 @@
                 <span>{{ post.downvotes }}</span>
               </v-col>
               <v-col class="text-right">
-                <v-btn text color="primary" @click="toggleComments(index)"><v-icon>mdi-comment</v-icon>
+                <v-btn
+                  text
+                  color="primary"
+                  @click="showComments[index] = !showComments[index]"
+                  ><v-icon>mdi-comment</v-icon>
                 </v-btn>
               </v-col>
             </v-row>
           </v-card-actions>
           <v-expand-transition>
-            <div v-show="showComments[index]">
+            <div v-show="show">
               <v-divider></v-divider>
               <v-card variant="tonal">
                 <div class="comment-section">
                   <h3>Comments</h3>
-                  <div v-for="(comment, index) in comments" :key="index" class="comment">
+                  <div
+                    v-for="(comment, index) in comments"
+                    :key="index"
+                    class="comment"
+                  >
                     <p>
                       <strong>{{ comment.author }}</strong> ({{
                         comment.timestamp
@@ -138,7 +88,8 @@
 export default {
   data() {
     return {
-      showComments: [],
+      show: false,
+      showComment: [false],
       comments: [],
       newComment: {
         author: "",
@@ -174,10 +125,6 @@ export default {
       ],
     };
   },
-  created() {
-  this.showComments = this.posts.map(() => false);
-},
-
   methods: {
     upvote(index) {
       this.posts[index].upvotes++;
@@ -185,8 +132,8 @@ export default {
     downvote(index) {
       this.posts[index].downvotes++;
     },
-    viewPost(post) { },
-    createPost() { },
+    viewPost(post) {},
+    createPost() {},
     addComment() {
       this.comments.push({
         author: "User",
@@ -200,9 +147,6 @@ export default {
     showReplyForm(index) {
       this.comments[index].showReplyForm = !this.comments[index].showReplyForm;
     },
-    toggleComments(index) {
-    this.$set(this.showComments, index, !this.showComments[index]);
-  },
     addReply(index) {
       this.comments[index].replyContent =
         this.comments[index].replyContent.trim();
@@ -245,7 +189,7 @@ h3 {
   font-size: 14px;
 }
 
-.comment>p:first-child {
+.comment > p:first-child {
   color: #3c3c3c;
   margin-bottom: 0.5em;
 }
